c20cb9ec85aa01b1ec0556ad126078c4df0cea4b
Pablo Werlang <pswerlang@gmail.com>
2025-06-15 17:21:33 -0300

    Fix error

--- index.js
+++ index.js
@@ -7,8 +7,10 @@
  */
 import WSClient from "./driver/wsclient.js";
 import WSServer from "./driver/wsserver.js";
+import Pledge from "./helper/pledge.js";
 
 export {
     WSClient,
     WSServer,
+    Pledge,
 };
